d5748e12a5782679246094d664724861c59f0a2f
mariaorfilaa <morfila19549@iesjoanramis.org>
2025-03-13 08:57:10 +0100

    Add files via upload

--- SantJordi/script.js
+++ SantJordi/script.js
@@ -3,7 +3,6 @@ const scoreDisplay = document.getElementById("score");
 const width = 28;
 let score = 0;
 const grid = document.querySelector (".grid");
-alert(score)
 
 //0 -pètal
 //1 - mur
@@ -12,11 +11,11 @@ alert(score)
 //4 - buit
 
 const layout = [
-    1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,2,2,2,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,
+    1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,
+    1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,0,2,2,2,1,1,1,2,2,2,0,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,2,2,2,2,2,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,
@@ -25,7 +24,7 @@ const layout = [
     1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
@@ -41,23 +40,53 @@ const layout = [
     1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,]
 
 //Create board
+let squares = [];
 
 function createBoard(){
     for(let i=0; i < layout.length; i ++){
         const square = document.createElement ('div')
-        if(layout[i]==1){
+        if(layout[i]==5){
             square.classList.add("brown")
         }else if (layout [i]==1){
-            square.classList.add("red")
+            square.classList.add("paret")
         }else if (layout [i]==2){
             square.classList.add("pink")
-        }else if (layout [i]==0){
+        }else if (layout [i]==3){
             square.classList.add("red")
-        }else if (layout [i]==4){
+        }else if (layout [i]==0){
             square.classList.add("green")
         }
         grid.appendChild (square);
+        squares.push(square)
     }
 }
 createBoard();
+
+console.log(squares)
+
+let posicioPrincep = 490;
+squares[posicioPrincep].classList.add("princep");
+
+function movePrincep(e){
+    squares[posicioPrincep].classList.remove("princep");
+        switch(e.key){
+            case 'ArrowLeft':
+                posicioPrincep -=1
+                break;
+            case 'ArrowRight':
+                posicioPrincep +=1
+                break;
+            case 'ArrowUp':
+                posicioPrincep -=28
+                break;
+            case 'ArrowDown':
+                posicioPrincep +=28
+                break;
+
+        }
+        squares[posicioPrincep].classList.add("princep");
+}
+
+document.addEventListener ('keyup', movePrincep);
+
 })
